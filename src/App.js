9eb93c6b202fea14860ac3331075b530aa0a70e1
James Hughes <jamhughes@deloitte.co.uk>
2021-11-22 13:15:27 +0000

    reducer for fun

--- src/App.js
+++ src/App.js
@@ -1,222 +1,237 @@
-import {useEffect, useState} from "react";
-import {Link, useParams} from "react-router-dom";
-
+import { useEffect, useReducer, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const GOOD_DONGERS = [
-    '( ● ‿ ● )',
-    '⊂(▀¯▀⊂)',
-    'ᕕ( ՞ ᗜ ՞ )ᕗ',
-    'ᕦ( ̿ ﹏ ̿ )ᕤ',
-    's( ^ ‿ ^)-b',
-    '(◉◞౪◟◉)',
-    '(ง ͠° ل͜ °)ง',
-    '(° ͜ʖ°)',
-    '(▀̿̿Ĺ̯̿̿▀̿ ̿)',
-    '♫ ┌༼ຈل͜ຈ༽┘ ♪',
-    '( ‾ʖ̫‾)',
-    '( ͡° ͜ʖ ͡°)',
-    '( ＾◡＾)',
-    'ԅ(ˆ⌣ˆԅ)',
-    '(ᵔᴥᵔ)',
-    'ಠ‿ಠ',
-    '( ◕ ◡ ◕ )',
-    '~(╯▽╰)~',
-    '~( ＾◡＾)~',
-    '┌( ͝° ͜ʖ͡°)ᕤ',
-    'ヽ༼ – ل͜ – ༽ﾉ',
-    '( ͡ᵔ ͜ʖ ͡ᵔ )',
-    '(͡◔ ͜ʖ ͡◔)',
-    '(=^-ω-^=)',
-    '~(˘◡˘~)',
-    '༼ᕗຈل͜ຈ༽ᕗ',
-    '(º◡º)っ',
-    '╰( ͡’◟◯ ͡’)╯',
-    'ヽ(”`▽´)ﾉ',
-    '(งಠل͜ಠ)ง',
-    '(っಠ‿ಠ)っ',
-    'ᕙ(▀̿̿Ĺ̯̿̿▀̿ ̿) ᕗ',
-    '(ง ͡ʘ ͜ʖ ͡ʘ)ง'
-
-]
+  "( ● ‿ ● )",
+  "⊂(▀¯▀⊂)",
+  "ᕕ( ՞ ᗜ ՞ )ᕗ",
+  "ᕦ( ̿ ﹏ ̿ )ᕤ",
+  "s( ^ ‿ ^)-b",
+  "(◉◞౪◟◉)",
+  "(ง ͠° ل͜ °)ง",
+  "(° ͜ʖ°)",
+  "(▀̿̿Ĺ̯̿̿▀̿ ̿)",
+  "♫ ┌༼ຈل͜ຈ༽┘ ♪",
+  "( ‾ʖ̫‾)",
+  "( ͡° ͜ʖ ͡°)",
+  "( ＾◡＾)",
+  "ԅ(ˆ⌣ˆԅ)",
+  "(ᵔᴥᵔ)",
+  "ಠ‿ಠ",
+  "( ◕ ◡ ◕ )",
+  "~(╯▽╰)~",
+  "~( ＾◡＾)~",
+  "┌( ͝° ͜ʖ͡°)ᕤ",
+  "ヽ༼ – ل͜ – ༽ﾉ",
+  "( ͡ᵔ ͜ʖ ͡ᵔ )",
+  "(͡◔ ͜ʖ ͡◔)",
+  "(=^-ω-^=)",
+  "~(˘◡˘~)",
+  "༼ᕗຈل͜ຈ༽ᕗ",
+  "(º◡º)っ",
+  "╰( ͡’◟◯ ͡’)╯",
+  "ヽ(”`▽´)ﾉ",
+  "(งಠل͜ಠ)ง",
+  "(っಠ‿ಠ)っ",
+  "ᕙ(▀̿̿Ĺ̯̿̿▀̿ ̿) ᕗ",
+  "(ง ͡ʘ ͜ʖ ͡ʘ)ง",
+];
 
 const BAD_DONGERS = [
+  "( ͡↑ ͜ʖ ͡↑)",
+  "╰[ ⁰﹏⁰ ]╯",
+  "ლ(ಥ Д ಥ )ლ",
+  "s( ^ ‸ ^)-p",
+  "ᕙ(◉෴◉)ᕗ",
+  "༼ ºل͟º༽",
+  "ʕ ͝°ل͟ ͝°ʔ",
+  "(⊙＿⊙’)",
+  "(╥﹏╥)",
+  "乁( •_• )ㄏ",
+  "(∩╹□╹∩)",
+  "(•_•)",
+  "(‘ºل͟º)",
+  "(つ 益 )つ",
+  "╭∩╮( ° ͜ʖ͡°)╭∩╮",
+  "ಠل͟ಠ",
+  "(╯_ʖ╰)",
+  "◕_◕",
+  "( ͝°_ʖ͡°)",
+  "ヽ(`Д´)ﾉ",
+  "(۞_۞)",
+  "(̿ಠ ̿Ĺ̯̿̿ಠ ̿)̄",
+  "(ᓄಠ_ಠ)ᓄ",
+  "(ºل͟º)",
+  "ノ(;Ĺ̯̿̿ ;ノ)",
+];
 
-    '( ͡↑ ͜ʖ ͡↑)',
-    '╰[ ⁰﹏⁰ ]╯',
-    'ლ(ಥ Д ಥ )ლ',
-    's( ^ ‸ ^)-p',
-    'ᕙ(◉෴◉)ᕗ',
-    '༼ ºل͟º༽',
-    'ʕ ͝°ل͟ ͝°ʔ',
-    '(⊙＿⊙’)',
-    '(╥﹏╥)',
-    '乁( •_• )ㄏ',
-    '(∩╹□╹∩)',
-    '(•_•)',
-    '(‘ºل͟º)',
-    '(つ 益 )つ',
-    '╭∩╮( ° ͜ʖ͡°)╭∩╮',
-    'ಠل͟ಠ',
-    '(╯_ʖ╰)',
-    '◕_◕',
-    '( ͝°_ʖ͡°)',
-    'ヽ(`Д´)ﾉ',
-    '(۞_۞)',
-    '(̿ಠ ̿Ĺ̯̿̿ಠ ̿)̄',
-    '(ᓄಠ_ಠ)ᓄ',
-    '(ºل͟º)',
-    'ノ(;Ĺ̯̿̿ ;ノ)'
-]
-
-const MAX_HEARTS = 6
+const MAX_HEARTS = 4;
 
 const random = (dongers) => {
-    return dongers[Math.floor(Math.random() * dongers.length)]
-}
+  return dongers[Math.floor(Math.random() * dongers.length)];
+};
 
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
 
 function permute(seed1, seed2) {
-    let permutations = []
-    for(let i = 0; i < seed1.length; i++) {
-        for(let j = 0; j < seed2.length; j++) {
-            permutations.push({
-                a: `${seed1[i]}`,
-                b: `${seed2[j]}`,
-                op: '✖',
-                ans: `${seed1[i] * seed2[j]}`
-            })
-        }
+  let permutations = [];
+  for (let i = 0; i < seed1.length; i++) {
+    for (let j = 0; j < seed2.length; j++) {
+      permutations.push({
+        a: `${seed1[i]}`,
+        b: `${seed2[j]}`,
+        op: "✖",
+        ans: `${seed1[i] * seed2[j]}`,
+      });
     }
+  }
 
-    return shuffle(permutations);
+  return shuffle(permutations);
 }
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TICK":
+      return { ...state, timer: state.timer + 1 };
+    case "ATTEMPT":
+      const newState = { ...state, value: "" };
+      const correct = action.value === state.question.ans;
+
+      if (correct) {
+        newState.reaction = random(GOOD_DONGERS);
+      } else {
+        const newLives = state.lives - 1;
+        if (newLives <= 0) {
+          newState.reaction = "( ✖ _ ✖ )";
+          newState.dead = true;
+        } else {
+          newState.reaction = random(BAD_DONGERS);
+        }
+
+        newState.lives = newLives;
+      }
+
+      const [next, ...rest] = state.questions;
+
+      if (next === undefined) {
+        newState.win = true;
+        newState.reaction = correct ? "WIN WIN WIN" : "U SURV1V3D";
+      } else {
+        newState.question = next;
+        newState.questions = rest;
+      }
+
+      return newState;
+    case "CHANGE":
+      return { ...state, value: action.value };
+    case "RESTART":
+      return init({ numbers: action.numbers, count: action.count });
+  }
+};
+
+const init = ({ numbers, count }) => {
+  const [first, ...rest] = permute(
+    numbers.split(",").map((n) => parseInt(n, 10)),
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  ).slice(0, parseInt(count, 10) || Infinity);
+
+  return {
+    question: first,
+    questions: rest,
+    reaction: "( O‿O )",
+    lives: MAX_HEARTS,
+    value: "",
+    dead: false,
+    win: false,
+    timer: 0,
+    total: rest.length + 1,
+  };
+};
 
 export function App() {
+  const { numbers, count } = useParams();
+  const [state, dispatch] = useReducer(reducer, { numbers, count }, init);
 
-    const { numbers, count } = useParams()
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (!state.dead && !state.win) {
+        dispatch({ type: "TICK" });
+      }
+    }, 1000);
+    return () => clearInterval(id);
+  });
 
-    const as = numbers.split(",").map(n => parseInt(n, 10))
+  return (
+    <div className="container">
+      <div className="stats">
+        <span className="counter">
+          {state.total - state.questions.length}/{state.total}
+        </span>
+        <span className="timer">
+          {new Date(state.timer * 1000).toISOString().substr(11, 8)}
+        </span>
+      </div>
 
-    const [first, ...rest] = permute(as, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).slice(0, parseInt(count, 10) || Infinity)
-    const [sum, setSum] = useState(first)
-    const [questions, setQuestions] = useState(rest)
-    const [reaction, setReaction] = useState("( O‿O )")
-    const [lives, setLives] = useState(MAX_HEARTS)
-    const [value, setValue] = useState('')
-    const [dead, setDead] = useState(false);
-    const [win, setWin] = useState(false);
-    const [timer, setTimer] = useState(0);
-
-    const total = rest.length + 1;
-
-    useEffect(() => {
-        const id = setInterval(() => {
-            if(!dead && !win) {
-                setTimer(timer + 1);
-            }
-        }, 1000)
-        return () => clearInterval(id)
-    })
-
-    const attempt = (value) => {
-        setValue('')
-
-        const correct = value === sum.ans
-        if(correct) {
-            setReaction(random(GOOD_DONGERS))
-        } else {
-            const newLives = lives - 1
-
-            if(newLives <= 0) {
-                setReaction('( ✖ _ ✖ )')
-                setDead(true);
-            } else {
-                setReaction(random(BAD_DONGERS))
-            }
-
-            setLives(newLives)
-
-        }
-
-        const [next, ...rest] = questions;
-
-        if(next === undefined) {
-            setWin(true)
-
-            setReaction(correct ? "WIN WIN WIN" : 'U SURV1V3D')
-        } else {
-            setSum(next);
-            setQuestions(rest);
-        }
-    }
-
-    return <div className="container">
-
-        <div className="stats">
-            <span className="counter">{total- questions.length}/{total}</span>
-            <span className="timer">{ new Date(timer * 1000).toISOString().substr(11, 8) }</span>
+      <div className="cool-guy-calc">
+        <div className="screen">
+          <div className="reaction">{state.reaction}</div>
+          <div className="lives">
+            {Array(MAX_HEARTS)
+              .fill(0)
+              .map((_, i) =>
+                i < state.lives ? (
+                  <span key={i}>♥</span>
+                ) : (
+                  <span key={i} className="depleted">
+                    ♥
+                  </span>
+                )
+              )}
+          </div>
         </div>
-
-        <div className="cool-guy-calc">
-            <div className="screen">
-                <div className="reaction">
-                    {reaction}
-                </div>
-                <div className="lives">
-                    {
-                        Array(MAX_HEARTS)
-                            .fill(0)
-                            .map((_, i) => i < lives ? (<span key={i}>♥</span>) : <span key={i} className="depleted">♥</span>)
-                    }
-                </div>
-            </div>
+      </div>
+      {!state.dead && !state.win && (
+        <div className="question-container">
+          <div className="question">
+            &gt; {state.question.a} {state.question.op} {state.question.b}
+          </div>
+          <div className="answer">
+            &gt;&nbsp;
+            <input
+              value={state.value}
+              onChange={(e) =>
+                dispatch({ type: "CHANGE", value: e.target.value })
+              }
+              onKeyPress={(e) =>
+                e.key === "Enter" &&
+                dispatch({ type: "ATTEMPT", value: e.target.value })
+              }
+              autoFocus={true}
+            />
+          </div>
         </div>
-        {
-            !dead && !win && (
-                <div className="question-container">
-                    <div className="question">&gt; {sum.a} {sum.op} {sum.b}</div>
-                    <div className="answer">&gt;&nbsp;
-                        <input value={value}
-                               onChange={(e) => setValue(e.target.value)}
-                               onKeyPress={(e) =>  e.key === 'Enter' && attempt(e.target.value)}
-                               autoFocus={true}/></div>
-                </div>
-            )
-        }
-        {
-            (dead || win) && (<>
-                    <div className="restart"><Link to="/">ᐊ</Link></div>
-                <div className="restart" onClick={() => {
-                    const [first, ...rest] = permute(as, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                    setSum(first)
-                    setQuestions(rest)
-                    setWin(false)
-                    setDead(false)
-                    setLives(MAX_HEARTS)
-                    setReaction("( O‿O )")
-                    setTimer(0);
-                }}>⟳</div>
-                </>
-            )
-        }
-        {
-            win && (
-                <div className="restart-message">C:&gt; WIN.BAT</div>
-            )
-        }
-        {
-            dead && (
-                <div className="restart-message">sudo rm -rf /</div>
-            )
-        }
-
+      )}
+      {(state.dead || state.win) && (
+        <>
+          <div className="restart">
+            <Link to="/">ᐊ</Link>
+          </div>
+          <div
+            className="restart"
+            onClick={() => dispatch({ type: "RESTART", numbers, count })}
+          >
+            ⟳
+          </div>
+        </>
+      )}
+      {state.win && <div className="restart-message">C:&gt; WIN.BAT</div>}
+      {state.dead && <div className="restart-message">sudo rm -rf /</div>}
     </div>
+  );
 }
